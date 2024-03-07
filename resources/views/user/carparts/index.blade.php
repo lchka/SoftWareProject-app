150ebd0ece75c0158ec4d4ed24ce44b9418115b8
Laura Hofmanova <N00222003@iadt.ie>
2024-03-07 18:47:31 +0000

    fixed route wrong controller name

--- resources/views/user/carparts/index.blade.php
+++ resources/views/user/carparts/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-bold text-xl text-white-800 leading-tight">
-            {{ __('All Car Parts') }}
+            {{ __('All USER Car Parts') }}
         </h2>
     </x-slot>
 
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('Admin/carparts', AdminCarPartController::class)->middleware(['auth'])->names('admin.carparts'); //middleware is responible for authencating and forcing the user to log in, removing this we can make it so that a user can be classed as 'guest' so they do not have to log in, unless they want too.
-Route::resource('User/carparts', AdminCarPartController::class)->middleware(['auth'])->names('user.carparts'); 
+Route::resource('User/carparts', UserCarPartController::class)->middleware(['auth'])->names('user.carparts'); 
 
 require __DIR__.'/auth.php';
