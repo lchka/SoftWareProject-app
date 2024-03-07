73e38dd47f7dc6883d37dcfb64f304f52823d199
Laura Hofmanova <N00222003@iadt.ie>
2024-03-07 18:36:28 +0000

    before running migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\CarPart;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        CarPart::factory()->count(15)->create();
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin= new Role ();
+        $role_admin->name = 'admin';
+        $role_admin->description = "Admin User";
+        $role_admin->save();
+
+        $role_user= new Role();
+        $role_user->name = 'user';
+        $role_user->description= "Ord User";
+        $role_user->save();
+
+    }
+}
